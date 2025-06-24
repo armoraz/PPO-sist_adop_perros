55bf7e080d0a710ab9f04c5129f2db0b8376fa0e
armoraz <oawind@gmail.com>
2025-06-24 13:49:02 -0300

    ajustes para railway

--- sistema_adopcion_web/sistema_adopcion_web/settings.py
+++ sistema_adopcion_web/sistema_adopcion_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t6(i5%2xel&fg^(yc)5%a#ty707xrghaxxye@koew+p604e4m7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
