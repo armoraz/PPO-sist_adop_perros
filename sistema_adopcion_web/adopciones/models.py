303d3227f6ce3b315e4be996c5ce00d126c7ae2e
armoraz <oawind@gmail.com>
2025-06-23 22:19:39 -0300

    cambio importantes en los modelos

--- sistema_adopcion_web/adopciones/models.py
+++ sistema_adopcion_web/adopciones/models.py
@@ -1,24 +1,56 @@
 from django.db import models
 
+# Choices predefinidos
+RAZAS = [
+    ('labrador', 'Labrador'),
+    ('beagle', 'Beagle'),
+    ('pastor_aleman', 'Pastor Alemán'),
+    ('poodle', 'Poodle'),
+    ('callejero', 'Callejero'),
+]
+
+SALUD = [
+    ('sano', 'Sano'),
+    ('tratamiento', 'En tratamiento'),
+    ('recuperacion', 'En recuperación'),
+]
+
+TAMAÑOS = [
+    ('pequeño', 'Pequeño'),
+    ('mediano', 'Mediano'),
+    ('grande', 'Grande'),
+]
+
+TEMPERAMENTOS = [
+    ('tranquilo','Tranquilo'),
+    ('jugueton','Juguetón'),
+    ('protector','Protector'),
+    ('sociable','Sociable'),
+]
+
+
 class Perro(models.Model):
     nombre = models.CharField(max_length=100)
-    raza = models.CharField(max_length=50)
+    raza = models.CharField(max_length=50, choices=RAZAS)
     edad = models.IntegerField()
-    tamaño = models.CharField(max_length=20, choices=[('pequeño', 'Pequeño'), ('mediano', 'Mediano'), ('grande', 'Grande')])
+    tamaño = models.CharField(max_length=20, choices=TAMAÑOS)
     peso = models.FloatField()
-    estado_salud = models.CharField(max_length=100)
+    estado_salud = models.CharField(max_length=100, choices=SALUD)
     vacunado = models.BooleanField(default=False)
-    estado = models.CharField(max_length=20, choices=[('disponible', 'Disponible'), ('reservado', 'Reservado'), ('adoptado', 'Adoptado')])
-    temperamento = models.CharField(max_length=50, choices=[('tranquilo','Tranquilo'), ('jugueton','Juguetón'), ('protector','Protector'), ('sociable','Sociable')])
-    reservado_por = models.ForeignKey(
-        'UsuarioAdoptante', null=True, blank=True, on_delete=models.SET_NULL
-    )
+    estado = models.CharField(max_length=20, choices=[
+        ('disponible', 'Disponible'),
+        ('reservado', 'Reservado'),
+        ('adoptado', 'Adoptado')
+    ])
+    temperamento = models.CharField(max_length=50, choices=TEMPERAMENTOS)
+    url_imagen = models.URLField(max_length=300, null=True, blank=True)
+    reservado_por = models.ForeignKey('UsuarioAdoptante', null=True, blank=True, on_delete=models.SET_NULL)
+
 
 class UsuarioAdoptante(models.Model):
     nombre = models.CharField(max_length=100)
     dni = models.CharField(max_length=20, unique=True)
     email = models.EmailField()
-    preferencias_raza = models.CharField(max_length=50, null=True, blank=True)
+    preferencias_raza = models.CharField(max_length=50, choices=RAZAS, null=True, blank=True)
     preferencias_edad = models.IntegerField(null=True, blank=True)
-    preferencias_tamaño = models.CharField(max_length=20, choices=[('pequeño', 'Pequeño'), ('mediano', 'Mediano'), ('grande', 'Grande')])
-
+    preferencias_tamaño = models.CharField(max_length=20, choices=TAMAÑOS)
